f22e50f701700e7ad0f55d91194ea98784397ead
hackrole <daipeng123456@gmail.com>
2015-03-02 23:14:50 +0800

    RandItem finish

--- RandomItems/RandomItems/main.m
+++ RandomItems/RandomItems/main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[])
 {
@@ -35,9 +36,57 @@ int main(int argc, const char * argv[])
             NSLog(@"%@", item);
         }
 
+        BNRItem *item = [[BNRItem alloc] init];
+
+        // 设置itemName, serialNumber, valueInDollars
+        [item setSerialNumber:@"Red Sofa"];
+        [item setSerialNumber:@"A1B2C"];
+        [item setValueInDollars:100];
+
+        // 使用.语法设置属性
+        item.itemName = @"Red Sofa 2";
+        item.serialNumber = @"A1B2C 2";
+        item.valueInDollars = 100;
+
+        NSLog(@"%@ %@ %@ %d", [item itemName], [item dateCreated],
+              [item serialNumber], [item valueInDollars]);
+        // 使用.语法打印
+        NSLog(@"%@ %@ %@ %d", item.itemName, item.dateCreated,
+              item.serialNumber, item.valueInDollars);
+
+        // 使用description方法
+        NSLog(@"%@", item);
+
+        // 使用初始化方法
+        BNRItem *itemInit = [[BNRItem alloc] initWithItemName:@"Red Line"
+                                                valueInDllars:102
+                                                 serialNumber:@"ABCD"];
+        NSLog(@"%@", itemInit);
+
+        BNRItem *itemWithName = [[BNRItem alloc] initWithItemName:@"Blue line"];
+        NSLog(@"%@", itemWithName);
+
+        BNRItem *itemNoName = [[BNRItem alloc] init];
+        NSLog(@"%@", itemNoName);
+
+        // 测试BNRItem
+        for (int i = 0; i< 10; i++) {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
+        }
+
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+
+        // 尝试抛出异常
+        //id lastObj = [items lastObject];
+        // [lastObj count];
 
         // 释放array对象
         items = nil;
+
+
         
     }
     return 0;
